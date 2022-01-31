00eda8491dd22edaa3418dccd14a7bd4c1cf98e8
jaehyeok3017 <gghh042404@gmail.com>
2022-01-31 22:01:09 +0900

    파일 수정 - item_main(padding값 적용), MainActivity(RecyclerView 관련)

--- app/src/main/java/com/example/picturediary/MainActivity.kt
+++ app/src/main/java/com/example/picturediary/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.picturediary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mainRecycler : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainRecycler = findViewById(R.id.mainRecyclerView)
+        mainRecycler.setOnClickListener {
+            // 화면 전환 (어떻게 할 지 모르겠어서 임시로 비워둠)
+        }
     }
 }
