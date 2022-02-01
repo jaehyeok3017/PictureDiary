5aec3d35993ddafdc71bf8161b6a51deedcb59ee
JiHyun13 <boksul1104@gmail.com>
2022-02-01 16:27:56 +0900

    add fab / 이제 난 main 안건드리고 일기작성 부분 만듭니다

--- app/src/main/java/com/example/picturediary/MainActivity.kt
+++ app/src/main/java/com/example/picturediary/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.picturediary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.example.picturediary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mainRecycler : RecyclerView
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
 
-        mainRecycler = findViewById(R.id.mainRecyclerView)
-        mainRecycler.setOnClickListener {
+        binding.mainRecyclerView.setOnClickListener {
             // 화면 전환 (어떻게 할 지 모르겠어서 임시로 비워둠)
+        }
+
+        binding.mainFab.setOnClickListener{
+            startActivity(Intent(this, MakeDiaryActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/com/example/picturediary/MakeDiaryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.picturediary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MakeDiaryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_diary)
+    }
+}
